5e452d0123c8f535b75f7e649342d18b00770735
u-sk <u.eggplant@gmail.com>
2019-08-08 11:00:30 +0900

    Initial Commit

---
+++ NewsReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by 板垣有祐 on 2019/08/08.
+//  Copyright © 2019 Swift-Beginner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
